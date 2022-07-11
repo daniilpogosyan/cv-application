4a5ecf1add16e88010df524918363e417a7f848c
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-07-11 18:20:47 +0500

    Create Personal component, add it to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import React from 'react';
+import Personal from './components/Personal';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      personal: {
+        name: "",
+        email: "",
+        phoneNumber: ""
+      },
+      education: [],
+      workExperience: []
+    }
+
+    this.handlePersonalChange = this.handlePersonalChange.bind(this);
+  }
+
+  handlePersonalChange(event) {
+    this.setState((state) => {
+      const personal = JSON.parse(
+        JSON.stringify(state.personal)
+      );
+      personal[event.target.name] = event.target.value;
+      return {personal}
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Personal
+          personal={this.state.personal}
+          onUpdate={this.handlePersonalChange}
+        />
+      </div>
+    );
+  }
 
 }
 
---
+++ src/components/Personal.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Field from './Field';
+
+export default class Personal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { name, email, phoneNumber } = this.props.personal;
+    const handleUpdate = this.props.onUpdate;
+
+    return (
+      <section>  
+        <h2>Personal Information:</h2>
+        <Field 
+          onChange={handleUpdate}
+          fieldName="Name:"
+          value={name}
+          varName="name"
+        /> 
+        <Field 
+          onChange={handleUpdate}
+          fieldName="Email:"
+          value={email}
+          varName="email"
+        /> 
+        <Field 
+          onChange={handleUpdate}
+          fieldName="Phone Number:"
+          value={phoneNumber}
+          varName="phoneNumber"
+        /> 
+      </section>
+    )
+  }
+}
+
